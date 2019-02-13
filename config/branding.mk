eff138ada010b9adf89713ef435784e1cea4931b
Sebastian (Felvesthe) <xdzienny@gmail.com>
2019-02-13 15:52:03 +0000

    Rebrand to DarkUnicorns
    Signed-off-by: Sebastian (Felvesthe) <xdzienny@gmail.com>

--- config/branding.mk
+++ config/branding.mk
@@ -1,30 +1,14 @@
 # Versioning System
-DU_BASE_VERSION = v13.0
-
-ifndef DU_BUILD_TYPE
-    DU_BUILD_TYPE := UNOFFICIAL
-endif
-
-# Only include DU-Updater for official, weeklies, and rc builds
-ifeq ($(filter-out OFFICIAL WEEKLIES RC,$(DU_BUILD_TYPE)),)
-    PRODUCT_PACKAGES += \
-        DU-Updater
-endif
-
-# Sign builds if building an official or weekly build
-ifeq ($(filter-out OFFICIAL WEEKLIES,$(DU_BUILD_TYPE)),)
-    PRODUCT_DEFAULT_DEV_CERTIFICATE := ../.keys/releasekey
-endif
+ANDROID_VERSION = 9.0
 
 # Set all versions
 DATE := $(shell date -u +%Y%m%d)
-TIME := $(shell date -u +%H%M)
-DU_VERSION := $(TARGET_PRODUCT)-$(DU_BASE_VERSION)-$(DATE)-$(TIME)-$(DU_BUILD_TYPE)
+DU_VERSION := $(TARGET_PRODUCT)-$(ANDROID_VERSION)-$(DATE)
 TARGET_BACON_NAME := $(DU_VERSION)
-ROM_FINGERPRINT := DirtyUnicorns/$(PLATFORM_VERSION)/$(DU_BUILD_TYPE)/$(DATE)$(TIME)
+ROM_FINGERPRINT := DarkUnicorns/$(PLATFORM_VERSION)/$(DATE)
 
 PRODUCT_PROPERTY_OVERRIDES += \
     BUILD_DISPLAY_ID=$(BUILD_ID) \
     ro.du.version=$(DU_VERSION) \
-    ro.mod.version=$(DU_BUILD_TYPE)-$(DU_BASE_VERSION)-$(DATE) \
+    ro.mod.version=$(ANDROID_VERSION)-$(DATE) \
     ro.du.fingerprint=$(ROM_FINGERPRINT)
